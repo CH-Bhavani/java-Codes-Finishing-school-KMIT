748a8bcedf949eb7de35927ce9f68b15e2cd13b6
Bhavani_CH <64600248+bhavani6353@users.noreply.github.com>
2021-12-14 20:54:50 +0530

    Add files via upload

---
+++ FascinatingNumber.java
@@ -0,0 +1,14 @@
+import java.util.*;
+	public class fascinatingnum{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		num=sc.nextInt();
+	int n1=num*2;
+	int n2=num*3;
+	String constr=String(num)+String(n1)+String(n2)cm;
+	int count=0;
+	for(char='1';char<='9';char++){
+		for(int i=0;i<constr.length;i++){
+			constr
+
+		
