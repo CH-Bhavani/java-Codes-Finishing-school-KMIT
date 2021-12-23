dd8401ff6386102a05f5589e1374a5747bac3602
Bhavani_CH <64600248+bhavani6353@users.noreply.github.com>
2021-12-23 21:57:16 +0530

    Create unsolved1.java

---
+++ unsolved1.java
@@ -0,0 +1,42 @@
+You are given an alphanumeric word W.
+You need to find the number of words that can be formed from W.
+
+The words have the following properties:
+    - It should be a substring of W of any length, minimum 1.
+    - All the letters in the word should be same.
+
+
+Input Format:
+-------------
+Single String W.
+
+Output Format:
+--------------
+Print an integer, the number of words can be formed
+
+
+Sample Input-1:
+---------------
+paddy
+ 
+Sample Output-1:
+----------------
+6
+
+Explanation:
+------------
+The words are : p, a, d, d, dd, y
+
+
+Sample Input-2:
+---------------
+abcde
+
+Sample Output-2:
+----------------
+5
+
+Explanation:
+------------
+The words are : a,b,c,d,e
+
