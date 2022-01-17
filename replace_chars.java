fcf31131dbf45341b5820ee04c7f88ec391b726d
Bhavani_CH <64600248+bhavani6353@users.noreply.github.com>
2022-01-17 19:56:20 +0530

    Create replace_chars.java

---
+++ replace_chars.java
@@ -0,0 +1,63 @@
+/*
+
+
+Ramu is given a pair of words, Now his teacher has given him a task. 
+His task is to see if the individual alphabets of the words are mapped properly or not 
+
+If they do so print true else print false
+
+Two strings are mapped properly  if the characters in 's' can be replaced to get 't'.
+All occurrences of a alphabet must be replaced with another alphabet while preserving the order of alphabets
+No two alphabets may map to the same alphabet but a alphabet may map to itself.
+
+
+Note:
+You may assume both s and t have the same length and all inputs contains('a-z')
+
+input = 
+egg
+app
+output = true
+
+input = 
+foo
+bar
+output = false
+
+input = 
+papel
+title
+output = true
+
+
+
+
+*/
+
+
+
+
+import java.util.*;
+public class test{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.next();
+        String s2=sc.next();
+        System.out.println(replacechars(s1,s2));
+    }
+    public static boolean replacechars(String s1,String s2){
+        HashMap<Character,Character> hs=new HashMap<>();
+        for(int i=0;i<s1.length();i++){
+            char c=s1.charAt(i);
+            char ch=s2.charAt(i);
+            if(!hs.containsKey(c))
+                if(!hs.containsValue(ch))
+                    hs.put(c,ch);
+                else
+                    return false;
+            else if (!hs.get(c).equals(ch))
+                return false;
+        }
+        return true;
+    }
+}
