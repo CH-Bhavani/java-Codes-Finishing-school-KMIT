b809c64d2da901fecf10afe22b92689d6afb802f
Bhavani_CH <64600248+bhavani6353@users.noreply.github.com>
2022-03-09 11:12:14 +0530

    Create Difficult_words.java

---
+++ Difficult_words.java
@@ -0,0 +1,33 @@
+/*
+
+                                                                                Tinku a Second Class Student is given a Phrase which contains few words in {}
+His teacher marked all dificult words in the { }. 
+His teacher gave him the meaning to the difficult words 
+he was asked to replaced the difficult words in the { } with the meanings given to him. 
+
+if the difficult word doesnt exist print ?
+
+The first line of input contains the phrase followed by difficult words 
+Each difficult words is given by a name followed by meaning (n difficult words)
+
+
+input={name}isa{gender}
+name ajay gender boy
+output=ajayisaboy
+
+
+input={name}is{age}yearsold
+name bob age two
+output=bobistwoyearsold
+
+
+input=hi{name}
+a b
+output=hi?
+
+
+
+
+*/
+
+
