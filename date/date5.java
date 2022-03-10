91dbf7e5a432ac261b153bea8ca3bb01a98012a2
Bhavani_CH <64600248+bhavani6353@users.noreply.github.com>
2022-03-10 20:36:44 +0530

    Add files via upload

---
+++ date/date5.java
@@ -0,0 +1,25 @@
+import java.time.*;
+
+class date5 {
+  public static void main(String[] args) {
+    LocalDate start = LocalDate.of(2015, Month.JANUARY, 1);
+    LocalDate end = LocalDate.of(2015, Month.MARCH, 30);
+    Period period = Period.ofMonths(1); // create a period
+
+    // period =Period.ofWeeks(3); 
+ //period=Period.ofDays(2);
+  //  peroid = Period.of(1, 2, 5);
+//	period = Period.ofYears(1); 
+    sendAlerts(start, end, period); 
+    System.out.println("start " + start);
+  }
+
+  private static void sendAlerts(LocalDate start, 
+    LocalDate end, Period period) 
+  { 
+    while (start.isBefore(end)) {
+      System.out.println("Reminder " + start);
+      start = start.plus(period); 
+    }  
+  }
+}
---
+++ date/date7.java
@@ -0,0 +1,24 @@
+import java.time.*;
+import java.time.format.*;
+
+class date7 {
+  public static void main(String[] args) {
+    LocalDate date1 = LocalDate.now();
+    LocalTime time1 = LocalTime.now();
+    LocalDateTime dateTime1 = LocalDateTime.now();
+	System.out.println(date1); 
+	System.out.println(time1); 
+	System.out.println(dateTime1); 
+    // LONG, MEDIUM, FULL, SHORT
+    DateTimeFormatter shortDateTime =
+      DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG);
+
+    System.out.println(shortDateTime.format(dateTime1)); 
+    System.out.println(shortDateTime.format(date1)); 
+   //System.out.println(shortDateTime.format(time1));
+
+    System.out.println(dateTime1.format(shortDateTime));
+    System.out.println(date1.format(shortDateTime));
+    //System.out.println(time1.format(shortDateTime));
+  } 
+}
