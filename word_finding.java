b95a8ba9e62f841b60aa718f6813441faa668cf8
Bhavani_CH <64600248+bhavani6353@users.noreply.github.com>
2022-04-02 17:57:44 +0530

    Create word_finding.java

---
+++ word_finding.java
@@ -0,0 +1,52 @@
+/*
+
+
+ajith is playing a game to find a words ,if he found the word return true othere wise return false.
+
+for example:
+
+input =
+3 3
+a b c
+d e a
+l m t
+cat
+output =true
+
+Note: 
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells 
+are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+
+example:2
+input =3 4
+a b c e
+s f c s
+a d e f
+abcced
+output =true
+
+
+input =3 4
+a b c e
+s f c s
+a d e e
+see
+output =true
+
+input =4 4
+a b c d
+e f g h
+i j k l
+m n o p
+abcdhlkjfm
+output =false
+
+
+
+
+
+*/
+
+
+
