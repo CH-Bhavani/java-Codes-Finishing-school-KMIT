7d4bcb0886209d8a7d023b9e61b1d2dd96b0e834
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-04-21 21:36:57 +0530

    Create 21-04-22.java

---
+++ 21-04-22.java
@@ -0,0 +1,23 @@
+/*
+
+
+srinivas is trying to rearrange characters in word based on frequency 
+of each character repeated number of times in that particular word.
+
+example:
+input =stff
+output =ffst
+
+
+input =fffbbb
+output =fffbbb
+
+both f and b appear three times , so answer would be fffbbb or bbbfff.
+return in insertion order of when they both are same frequency. 
+ 
+input =caAc
+output =ccaA
+
+
+
+*/
