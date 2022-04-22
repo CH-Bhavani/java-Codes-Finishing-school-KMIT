cfd8f39ac7be75165a041f9779d3b91e4b307207
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-04-22 10:54:13 +0530

    Update 21-04-22.java

--- 21-04-22.java
+++ 21-04-22.java
@@ -21,3 +21,34 @@ output =ccaA
 
 
 */
+
+
+
+
+import java.util.*;
+public class test{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        System.out.println(rearrangeChar(s));
+}
+public static String rearrangeChar(String w){
+    LinkedHashMap<Character,Integer> hm=new LinkedHashMap<>();
+    HashMap<Character,Integer> ind=new HashMap<>();
+    for(int i=0;i<w.length();i++){
+        char c=w.charAt(i);
+        if(!ind.containsKey(c))
+            ind.put(c,i);
+        hm.put(c,hm.getOrDefault(c,0)+1);
+    }
+    System.out.println(hm+" "+ind);
+    PriorityQueue<Character> pq=new PriorityQueue<>((a,b)->hm.get(b)-hm.get(a));
+    for(char i:w.toCharArray())
+        pq.offer(i);
+    StringBuilder sb=new StringBuilder();
+    for(char c:pq){
+        sb.append(c);
+    }
+    return sb.toString();
+}
+}
