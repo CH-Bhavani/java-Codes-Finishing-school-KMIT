574eb5ac16f091e846e1c6a0a37436c38350d3c3
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-04-27 21:13:13 +0530

    Create heights&Items.java

---
+++ heights&Items.java
@@ -0,0 +1,129 @@
+/*
+
+A courier company has to deliver N items with different heights.
+All the items have to deliver to one building only.
+The building has K rooms inside indexed 0,1,...,K-1, and the heights of 
+the rooms are different. All the rooms connected with each other and has 
+two entry points, one entry is at frontside and other entry is at backside of 
+the building . e.g, you can reach room-1 from room-0 only, room-2 from 
+room-1 only, ..so on, and vice-versa.
+
+Now the task of the delivery agent is to keep the items inside the building.
+
+To keep the items into the rooms, delivery agent has to follow these rules:
+ - Agent can't keep, two goods in one room.
+ - Agent can enter into the building either from frontside or from back side,
+   If the height of some room is less than the height of an item,
+   then the item will be stopped before that room, so are the items behind it.
+ - Agent can rearrange the order of items to keep them inside the building.
+   
+Your task is to return the maximum number of items, the delivery agent can keep 
+inside the building.
+
+Input Format:
+-------------
+Line-1 -> two integers N and K, number of items and number of rooms.
+Line-2 -> N space separated integers, heights of the items.
+Line-3 -> K space separated integers, heights of the rooms.
+
+Output Format:
+--------------
+Print an integer as result.
+
+
+Sample Input-1:
+---------------
+5 4
+2 3 3 4 5
+4 5 2 3
+
+Sample Output-1:
+----------------
+4
+
+Explanation:
+------------
+We can first keep the item of height 2 in room 2. 
+Then we can keep the item of height 3 in room 3.
+Then we can keep the item of height 3 in room 1.
+Then we can keep the item of height 4 in room 0.
+
+
+Sample Input-2:
+---------------
+4 5
+4 6 6 3
+3 2 4 5 6
+
+Sample Output-2:
+----------------
+3
+Explanation:
+------------
+Notice that it's not possible to keep the two items of height 6 into the building,
+since there's only 1 room of height >= 6
+Other valid solutions are to put the item with height 4 in room 2 or 
+to put the item with height 3 in room 2 or room 0 before putting 
+the the item with height 4 and 6.
+
+
+*/
+
+
+import java.util.*;
+public class test{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] items = new int[n];
+        int[] rooms = new int[k];
+        for(int i=0;i<n;i++) 
+            items[i] = sc.nextInt();
+        for(int i=0;i<k;i++) 
+            rooms[i] = sc.nextInt();
+        int[] left = cal_left(rooms,k);
+        int[] right = cal_right(rooms,k);
+         int result[] = new int[k];
+        for(int i=0;i<k;i++){
+            result[i] = Math.max(right[i],left[i]);
+        }
+        System.out.println(total(result,items));
+            
+}
+    public static int[] cal_left(int[] rooms,int k){
+        
+        int[] left = new int[k];
+        int l = Integer.MAX_VALUE;
+        for(int i=0;i<k;i++){
+            l = Math.min(l,rooms[i]);
+            left[i] = l;
+        }
+        return left;
+    }
+    public static int[] cal_right(int[] rooms,int k){
+        int[] right = new int[k];
+        int r = Integer.MAX_VALUE;
+        for(int i=k-1;i>=0;i--){
+            r = Math.min(r,rooms[i]);
+            right[i] = r;
+        }
+        return right;
+    }
+    public static int total(int[] result,int[] items){
+        Arrays.sort(result);
+        Arrays.sort(items);
+        int i=0,c=0,j=0;
+        while(i<result.length&& j<items.length ){
+            if(result[i]<items[j]){
+                i++;
+            }
+            else{
+                c++;
+                i++;
+                j++;
+            }
+        }
+        return c;
+    }
+}
