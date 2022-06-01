039a25a0a6abe4dd937a53f33bfc7258cd3df880
Bhavani <64600248+CH-Bhavani@users.noreply.github.com>
2022-06-01 21:03:08 +0530

    Create 01-06-22.java

---
+++ 01-06-22.java
@@ -0,0 +1,36 @@
+/*
+
+write a program to display the nth term in a fibonacci series
+Use BigInteger
+
+
+Fibonacci Series - 0 1 1 2 3 5 and so on. 
+
+input = 5
+output = 3
+
+
+
+
+*/
+
+
+import java.util.*;
+import java.math.*;
+public class Fibonacci{
+    public static BigInteger fib(int n){
+        BigInteger a = BigInteger.valueOf(0);
+        BigInteger b = BigInteger.valueOf(1);
+        BigInteger c = BigInteger.valueOf(1);
+        for (int j=2 ; j<n ; j++){
+            c =  a.add(b);
+            a = b;
+            b = c;
+        }
+        return (b);
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println(fib(sc.nextInt()));
+    }
+}
